8e743ffae2f6b5d3fbfa3ce6b165dd2f15c6239f
encloinc <jelch2002@gmail.com>
2016-08-27 21:53:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,64 +1,38 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+# To use a consistent encoding
 from codecs import open
 from os import path
+
 here = path.abspath(path.dirname(__file__))
 
+# Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='ESF',
 
-    
     version='1.0.0',
 
-    description='The easiest way to store data on arrays',
+    description='ESF, the easy way to write and save files in an array format',
     long_description=long_description,
 
-    url='https://github.com/encloinc/esf',
+    url='https://github.com/encloinc/esf/',
 
     author='EncloCreations',
     author_email='jelch2002@gmail.com',
 
-    license='MIT',
-
-
     classifiers=[
-
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
 
-    keywords='files esf arrays',
-    packages=['none'],
+    keywords=['files', 'saving', 'arrays'],
 
-    install_requires=[],
+    py_modules=["esf"],
 
+    install_requires=[''],
 
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
-
-    package_data={
-        'esf': ['esf'],
-    },
-
-  
-    data_files=[('my_data', ['data/data_file'])],
-
-    entry_points={
-        'console_scripts': [
-            'sample=sample:main',
-        ],
-    },
 )
